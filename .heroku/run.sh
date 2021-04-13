d25d8a851ce7f1a03ea16878b11cf8d26b2d59fa
muxueqz <zhangmingyuan240@gmail.com>
2021-04-13 16:31:50 +0800

    first commit

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -5,7 +5,8 @@ VER=4.23.1
 
 mkdir v2raybin
 cd v2raybin
-curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+# curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip
 chmod +x v2ray
 rm -rf v2ray.zip
