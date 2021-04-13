65adc663d32e8696189003d1d538183e93de7456
muxueqz <zhangmingyuan240@gmail.com>
2021-04-13 16:05:39 +0800

    first commit

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -4,9 +4,11 @@ VER=4.37.3
 unzip --help
 curl --help
 
-curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip \
-#mkdir /v2raybin && cd /v2raybin \
-# && curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip \
-# && unzip v2ray.zip \
-# && mv /v2raybin/v2ray-v$VER-linux-64/v2ray /v2raybin/ \
+#curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip \
+mkdir /v2raybin && \
+  cd /v2raybin && \
+  curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip && \
+  unzip v2ray.zip && \
+  echo
+  # mv /v2raybin/v2ray-v$VER-linux-64/v2ray /v2raybin/ \
 # && mv /v2raybin/v2ray-v$VER-linux-64/v2ctl /v2raybin/
