6c802c25e310a06739e365152427f9cfe9341ec4
muxueqz <zhangmingyuan240@gmail.com>
2021-04-13 16:15:10 +0800

    first commit

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
 VER=4.37.3
-unzip --help
-curl --help
 
+ls -alht
 pwd
-mkdir /v2raybin
-cd /v2raybin
+mkdir /tmp/build/v2raybin
+cd /tmp/build/v2raybin
 curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip
-chmod +x /v2raybin/v2ray
+chmod +x /tmp/build/v2raybin/v2ray
 rm -rf v2ray.zip
 
-cd /v2raybin
 echo -e -n "$CONFIG_JSON1" > config.json
 echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-echo -e "$CERT_PEM" > cert.pem
-echo -e "$KEY_PEM"  > key.pem
+  echo -e "$CERT_PEM" > cert.pem
+  echo -e "$KEY_PEM"  > key.pem
 fi
 ./v2ray
