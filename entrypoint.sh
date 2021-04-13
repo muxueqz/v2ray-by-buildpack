b2ad42ae8a19bee2528012f2f102482ed27c9b0b
muxueqz <zhangmingyuan240@gmail.com>
2021-04-13 16:21:45 +0800

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 cd v2raybin
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > cert.pem
-  echo -e "$KEY_PEM"  > key.pem
-fi
+
+echo -e -n "$CONFIG_JSON" | sed "s/HEROKU_PORT/$PORT" > config.json
 ./v2ray
 
